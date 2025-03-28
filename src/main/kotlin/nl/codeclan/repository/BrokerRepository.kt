07a52fe5073f91677ba73143a8c3967ab77eed16
Jan-Peter Ruiter <github@gorgyra.com>
2025-03-28 16:27:34 +0100

    Add address to Broker Repository

--- src/main/kotlin/nl/codeclan/repository/BrokerRepository.kt
+++ src/main/kotlin/nl/codeclan/repository/BrokerRepository.kt
@@ -1,5 +1,6 @@
 package nl.codeclan.repository
 
+import nl.codeclan.model.Address
 import nl.codeclan.model.Broker
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -11,8 +12,8 @@ interface BrokerRepository : CrudRepository<Broker, Long> {
     @Transactional
     @Modifying
     @Query(
-        """update Broker c set c.name = ?1, c.contactName = ?2, c.contactNumber = ?3
+        """update Broker c set c.name = ?1, c.contactName = ?2, c.address = ?3, c.contactNumber = ?4
             where c.id = ?4"""
     )
-    fun updateBroker(name: String, contactName: String, contactNumber: String, id: Long): Int
+    fun updateBroker(name: String, contactName: String, contactNumber: String, address: Address, id: Long): Int
 }
